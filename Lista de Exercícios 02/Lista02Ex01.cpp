9025d9663562977b62c20752a114071151d72b0c
Débora Yamao <89618716+DeboraYamao@users.noreply.github.com>
2021-09-16 14:04:52 -0300

    Update Lista02Ex01.cpp

--- Lista de Exercícios 02/Lista02Ex01.cpp
+++ Lista de Exercícios 02/Lista02Ex01.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <locale.h>
+//Apresentar todos os n√∫meros divis√≠veis por 4 que sejam menores que 200.
 
 int i;
 
@@ -8,7 +9,7 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	for(i=0;i<=200;i++){
 		if(i!=0 && i%4==0){
-			printf("O n˙mero %d È divisÌvel por 4\n",i);
+			printf("O n√∫mero %d √© divis√≠vel por 4\n",i);
 		}
 	}
 	return(0);

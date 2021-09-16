3d6cbb77bea16ab388c24d7d2f2dc18272c39264
Débora Yamao <89618716+DeboraYamao@users.noreply.github.com>
2021-09-16 14:11:17 -0300

    Update Lista02Ex04.cpp

--- Lista de Exercícios 02/Lista02Ex04.cpp
+++ Lista de Exercícios 02/Lista02Ex04.cpp
@@ -1,3 +1,4 @@
+//Receber um nÃºmero do teclado e informar se ele Ã© divisÃ­vel por 10, por 5, por 2 ou se nÃ£o Ã© divisÃ­vel por nenhum destes.
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
@@ -6,19 +7,19 @@ int num;
 
 int main(){
 	setlocale(LC_ALL,"Portuguese");
-	printf("Digite um número: ");
+	printf("Digite um nÃºmero: ");
 	scanf("%d",&num);
 	if(num%10==0){
-		printf("\nO número %d é dividível por 10.",num);
+		printf("\nO nÃºmero %d Ã© dividÃ­vel por 10.",num);
 		
 	}if(num%5==0){
-		printf("\nO número %d é dividível por 5.",num);
+		printf("\nO nÃºmero %d Ã© dividÃ­vel por 5.",num);
 		
 	}if(num%2==0){
-		printf("\nO número %d é dividível por 2.",num);
+		printf("\nO nÃºmero %d Ã© dividÃ­vel por 2.",num);
 	
 	}else{
-		printf("\nO número %d não é dividível por 10, 5 e 2.",num);
+		printf("\nO nÃºmero %d nÃ£o Ã© dividÃ­vel por 10, 5 e 2.",num);
 	}
 	return(0);
 }

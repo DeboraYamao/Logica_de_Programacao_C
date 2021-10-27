3d0b19dc7b38fb107b60fcc2b4338d93802d84f5
Débora Yamao <89618716+DeboraYamao@users.noreply.github.com>
2021-10-26 22:30:18 -0300

    Arquivo da aula 26/10/2021
    Inclusão da opção de atualizar o projeto cadastrado

--- Diversos/cadastro_projeto.cpp
+++ Diversos/cadastro_projeto.cpp
@@ -12,8 +12,17 @@ struct projeto{
 };
 
 struct projeto projetos[200];//vetor projetos com atributos do projeto
-int opcao,sequencia=0;
+int opcao,sequencia=0,cont=0,valida=0;
 const int A=1,F=2,C=3;
+
+void dados_tela(int i){
+	printf("\nCódigo: %d ",projetos[i].codigo);
+	printf("\nTítulo: %s ",projetos[i].titulo);
+	printf("\nDescrição: %s ",projetos[i].descricao);
+	printf("\nResponsável: %s ",projetos[i].responsavel);
+	printf("\nStatus: %d ",projetos[i].status);
+	printf("\n\n****************************************\n");
+}
 
 void cadastrar_projeto(){
 	bool cadastro=true;
@@ -34,31 +43,27 @@ void cadastrar_projeto(){
 		fflush(stdin);
 		gets(projetos[sequencia].responsavel);
 		
-		printf("\nDigite o status do Projeto: ");
+		printf("\nInforme o status do Projeto:\n1 - A iniciar\n2 - Em execução\n3 - Concluído\nStatus: ");
 		fflush(stdin);
 		scanf("%d",&projetos[sequencia].status);
-		
+	
 		printf("\nCadastro realizado com sucesso!\n\nDeseja realizar um novo cadastro? S-Sim N-Não ");
 		fflush(stdin);
 		scanf("%c",&continuar);
-		if((continuar=='N') || (continuar=='n')){
+		if((continuar=='N') or (continuar=='n')){
 			cadastro=false;
 		}
 		sequencia++;
+		cont+=1;
 	}while(cadastro==true);
 }
 
 void todos_projetos(){
 	system("cls");
 	int i;
-	if(projetos[0].codigo>0){
+	if(cont>0){
 		for(i=0;i<sequencia;i++){
-			printf("\nCódigo: %d ",projetos[i].codigo);
-			printf("\nTítulo: %s ",projetos[i].titulo);
-			printf("\nDescrição: %s ",projetos[i].descricao);
-			printf("\nResponsável: %s ",projetos[i].responsavel);
-			printf("\nStatus: %d ",projetos[i].status);
-			printf("\n****************************************\n");
+			dados_tela(i);
 		}
 	}else{
 		printf("\nNenhum projeto cadastrado.");
@@ -69,29 +74,74 @@ void todos_projetos(){
 void por_status(int s){
 	system("cls");
 	int i;
-	bool achou;
-	for(i=0;i<sequencia;i++){
-		if(projetos[i].status==s){
-			printf("\nCódigo: %d ",projetos[i].codigo);
-			printf("\nTítulo: %s ",projetos[i].titulo);
-			printf("\nDescrição: %s ",projetos[i].descricao);
-			printf("\nResponsável: %s ",projetos[i].responsavel);
-			printf("\nStatus: %d ",projetos[i].status);
-			printf("\n****************************************\n");
-			achou=true;
-		}else{
-			achou=false;
-			
+	if(cont>0){
+		for(i=0;i<sequencia;i++){
+			if(projetos[i].status==s){
+				dados_tela(i);
+			}
 		}
-	}
-	if(achou==false){
-		printf("\nProjeto não encontrado!");
+	}else{
+		printf("\nNenhum projeto encontrado.");
 	}
 	system("pause");
 }
 
 busca_codigo(){
-	
+	int i, busca=0;
+	system("cls");
+	printf("\nInforme o código de busca: ");
+	scanf("%d",&busca);
+	if(cont>0){
+		for(i=0;i<sequencia;i++){
+			if(projetos[i].codigo==busca){
+				dados_tela(i);
+			}
+		}
+	}else{
+		printf("\nNenhum projeto encontrado.");
+	}
+	system("pause");
+}
+
+void atualizar_projeto(){
+	int i, busca=0;
+	bool cadastro=true;
+	char continuar;
+	system("cls");
+	printf("\nInforme o código de busca: ");
+	scanf("%d",&busca);
+	if(cont>0 and busca<=sequencia){
+		for(i=0;i<sequencia;i++){
+			if(projetos[i].codigo==busca){
+				dados_tela(i);
+				
+				projetos[busca-1].codigo;
+				
+				printf("\nDigite o título do Projeto atualizado: ");
+				fflush(stdin);
+				gets(projetos[busca-1].titulo);
+				
+				printf("\nDigite a descrição do Projeto atualizado: ");
+				fflush(stdin);
+				gets(projetos[busca-1].descricao);
+				
+				printf("\nDigite o responsável do Projeto atualizado: ");
+				fflush(stdin);
+				gets(projetos[busca-1].responsavel);
+				
+				printf("\nInforme o status do Projeto atualizado:\n1 - A iniciar\n2 - Em execução\n3 - Concluído\nStatus: ");
+				fflush(stdin);
+				scanf("%d",&projetos[busca-1].status);
+			
+				printf("\nAtualização realizada com sucesso!\n");
+				system("pause");
+			}
+		}
+	}else{
+		printf("\nNenhum projeto encontrado.\n");
+		system("pause");
+	}
+
 }
 
 int main(){
@@ -99,7 +149,7 @@ int main(){
 	do{
 		system("cls");
 		opcao=9;
-		printf("\nDigite:\n1 - Cadastrar projetos\n2 - Imprimir todos os projetos\n3 - Imprimir todos os projetos com status: A INICIAR\n4 - Imprimir todos os projetos com status: EM EXECUÇÂO\n5 - Imprimir todos os projetos com status: CONCLUÍDO\n6 - Busca de projeto por código\n0 - Sair da aplicação\nOpção: ");
+		printf("\nOpções:\n\n1 - Cadastrar projetos\n2 - Imprimir todos os projetos\n3 - Imprimir todos os projetos com status: A INICIAR\n4 - Imprimir todos os projetos com status: EM EXECUÇÂO\n5 - Imprimir todos os projetos com status: CONCLUÍDO\n6 - Busca de projeto por código\n7 - Atualizar Projeto \n0 - Sair da aplicação\n\nOpção selecionada: ");
 		scanf("%d",&opcao);
 		
 		switch (opcao){
@@ -124,19 +174,22 @@ int main(){
 			break;
 			
 			case 6:
-				//busca_codigo();
+				busca_codigo();
+			break;
+			
+			case 7:
+				atualizar_projeto();
 			break;
 			
 			case 0:
-				
+				printf("\nEncerrando a aplicação!");
 			break;
 			
 			default:
-				printf("\nOpção inválida!");
+				printf("\nOpção inválida!\n");
 				system("pause");
 		}
 		
 	}while(opcao!=0);
 return 0;	
 }
-
